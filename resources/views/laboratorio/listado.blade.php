421ea9310464184c9fec6699db881552c2b29d69
panchitopls <francisco.sepulvedaaa@gmail.com>
2020-08-13 23:56:47 -0400

    Botones Eliminar

--- resources/views/laboratorio/listado.blade.php
+++ resources/views/laboratorio/listado.blade.php
@@ -85,7 +85,9 @@
                                     </svg>
                                 </button>
                             </a>
-                            <a href="{{ route('laboratorio.delete.route', $lab->codigoLab) }}">
+
+                            <a title="Borrar" href="{{ route('laboratorio.delete.route', $lab->codigoLab) }}" title="delete" class="delete" onclick="
+                                return confirm('ESTA SEGURO QUE DESEA ELIMINAR ESTE LABORATORIO?')">
                                 <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="black" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
--- resources/views/user/listado.blade.php
+++ resources/views/user/listado.blade.php
@@ -35,37 +35,14 @@
                             </a>
 
                             <!-- Boton Eliminar -->
-                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                </svg>
-                            </button>
-
-                            <!-- Ventana de Confirmacion -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">¿Estás seguro que deseas ELIMINAR el usuario?</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            ARREGLAR ... Para eliminar por favor presione Confirmar...
-                                        </div>
-                                        <div class="modal-footer">
-                                            <a href="{{ url('/Usuarios') }}">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                            </a>
-                                            <a href="{{ route('user.delete.route', $user->rut) }}">
-                                            <button type="button" class="btn btn-primary">Confirmar</button>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <a title="Borrar" href="{{ route('user.delete.route', $user->rut) }}" title="delete" class="delete" onclick="
+                                return confirm('ESTA SEGURO QUE DESEA ELIMINAR ESTE USUARIO?')">
+                                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                            </svg>
+                                        </button></a>
                         </td>
                     </tr>
                 @endif
