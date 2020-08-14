f8c5ea36d3ef4dae90d33ef23a34ad9b5fc20441
panchitopls <francisco.sepulvedaaa@gmail.com>
2020-08-13 23:56:32 -0400

    Revert "botones eliminar"
    This reverts commit 697cdc7ca52aebcfa510aedd6284686b518fd203.

--- resources/views/laboratorio/detalles.blade.php
+++ resources/views/laboratorio/detalles.blade.php
@@ -50,7 +50,6 @@
                                     <div class="custom-control custom-checkbox">
                                         <input type="checkbox" class="custom-control-input" id="check1" name="modulosReservados" value="1 Lunes">
                                         <label class="custom-control-label" for="check1">Reservar</label>
-                                        
                                     </div>
                                 </li>
                             </td>
