277155873098d11341ca00b0c1e6e1cd4bf73780
Musdate <bastiantroncoso97@gmail.com>
2020-10-05 15:51:13 -0300

    database/seeds/DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,8 +6,6 @@ class DatabaseSeeder extends Seeder{
     
     public function run(){
 
-        
-
         $this->call(TipoUsuarioSeed::class);
         $this->call(CuentasSeed::class);
         $this->call(LaboratorioSeed::class);
