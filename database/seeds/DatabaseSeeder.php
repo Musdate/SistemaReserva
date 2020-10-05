8c8da0fedf4d8aa4f9583070055e4f6553446676
Musdate <bastiantroncoso97@gmail.com>
2020-10-05 14:52:16 -0300

    database/seeds/DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,14 +6,7 @@ class DatabaseSeeder extends Seeder{
     
     public function run(){
 
-        //Vaciar tablas antes de insertar
-        DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
-        DB::table('reservas')->truncate();
-        DB::table('users')->truncate();
-        DB::table('laboratorios')->truncate();
-        DB::table('tipousuario')->truncate();
-        DB::table('modulos_reservados')->truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+        
 
         $this->call(TipoUsuarioSeed::class);
         $this->call(CuentasSeed::class);
