d2b457b7ae820e9594ad166c065dda8ef93392e4
Francisco <francisco.sepulvedaaa@gmail.com>
2020-11-16 01:49:48 -0300

    Ultimos cambios de diseño

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,12 +20,137 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Exo&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
     <!-- Styles -->
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
 
 <body>
+<header>
+        <div class="navbar navbar-expand-md navbar-dark fixed-top bg-dark navbar-inverse">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">
+                    <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-house" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                        <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                    </svg>
+                </a>
+              </div>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent"> 
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        
+                    @else
+                </ul>
+
+                <!-- Left Side Of Navbar -->
+
+                <!-- Si es Admin -->
+                @if ( Auth::user()->tipoUsuarioID == '0' )
+                    <ul class="nav navbar-nav">
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Laboratorios
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="{{ url('/AgregarLaboratorio') }}">Agregar Laboratorio</a>
+                                    <a class="dropdown-item" href="{{ url('/Laboratorios') }}">Listado de Laboratorios</a>
+                                </div>
+                            </div>
+
+                        <li class="nav-item">
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Encargados
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="{{ url('/AgregarEncargado') }}">Agregar Encargado</a>
+                                    <a class="dropdown-item" href="{{ url('/Encargados') }}">Listado de Encargados</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Usuarios
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="{{ url('/AgregarUsuario') }}">Agregar Usuario</a>
+                                    <a class="dropdown-item" href="{{ url('/Usuarios') }}">Listado de Usuarios</a>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Administrador
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="{{ url('/AgregarAdmin') }}">Agregar Administrador</a>
+                                <a class="dropdown-item" href="{{ url('/Admins') }}">Lista de Administradores</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            @endif
+
+            <!-- Si es Encargado -->
+            @if ( Auth::user()->tipoUsuarioID == '1' )
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Usuarios
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="{{ url('/AgregarUsuario') }}">Agregar Usuario</a>
+                                <a class="dropdown-item" href="{{ url('/Usuarios') }}">Listado de Usuarios</a>
+                            </div>
+                          </div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Laboratorios
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="{{ url('/Laboratorios') }}">Listado de Laboratorios</a>
+                            </div>
+                          </div>
+                    </li>
+                </ul>
+            @endif
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->nombre }} <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('user.details.route', Auth::user()->rut) }}">
+                            Perfil
+                        </a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                            Desconectarse
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+             </ul>   
+            @endguest
+        </div>
+    </div>
+</header>
+    
+  <main class="py-4"></br></br></br>
+    <div class="container">
     </br></br>
     <div class="container">
     <div class="card"></div>
@@ -65,5 +190,9 @@
         <a href="/"><button><span>Volver</span></button></a>
       </div>
     </div>
-  </div>
+  </div></div>
+</main>
+
 </body>
+
+</html>
